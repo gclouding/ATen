8f69337984b97f3f169178ade98a02d4c59d8e49
Zach DeVito <zdevito@fb.com>
2017-10-24 12:11:55 -0700

    directory restructure

--- tools/subtree_dir_merge_all.sh
+++ tools/subtree_dir_merge_all.sh
@@ -2,9 +2,9 @@ set -e
 set -x
 DIR=$(dirname $0)
 git fetch pytorch
-$DIR/subtree_dir_merge.sh pytorch/master torch/lib/TH lib/TH
-$DIR/subtree_dir_merge.sh pytorch/master torch/lib/THC lib/THC
-$DIR/subtree_dir_merge.sh pytorch/master torch/lib/THNN lib/THNN
-$DIR/subtree_dir_merge.sh pytorch/master torch/lib/THCUNN lib/THCUNN
-$DIR/subtree_dir_merge.sh pytorch/master torch/lib/THS lib/THS
-$DIR/subtree_dir_merge.sh pytorch/master torch/lib/THCS lib/THCS
+$DIR/subtree_dir_merge.sh pytorch/master torch/lib/TH src/TH
+$DIR/subtree_dir_merge.sh pytorch/master torch/lib/THC src/THC
+$DIR/subtree_dir_merge.sh pytorch/master torch/lib/THNN src/THNN
+$DIR/subtree_dir_merge.sh pytorch/master torch/lib/THCUNN src/THCUNN
+$DIR/subtree_dir_merge.sh pytorch/master torch/lib/THS src/THS
+$DIR/subtree_dir_merge.sh pytorch/master torch/lib/THCS src/THCS
